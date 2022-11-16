081b98458f641d46ddec8a035ea2af09371eef6e
Strift <laurent@twic.pics>
2022-11-16 11:35:37 +0100

    Disable vue shim generation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  // https://v3.nuxtjs.org/getting-started/installation#prerequisites
+  // If you enabled ‘Take Over Mode’ or installed the ‘TypeScript Vue Plugin (Volar)’ extension
+  typescript: {
+    shim: false
+  },
   modules: [
     '@nuxtjs/strapi'
   ],
